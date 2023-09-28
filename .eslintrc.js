56a9604d5fb6566dffd917f2f4bc421b30760ee4
Alex Korzun <koleksandr4@gmail.com>
2023-09-28 18:46:49 +0300

    refactored code, added methods to the object

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true
+	},
+	"extends": "eslint:recommended",
+	"overrides": [
+	],
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": "off",
+		"linebreak-style": [
+			"error",
+			"windows"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
+};
--- js/script.js
+++ js/script.js
@@ -1,65 +1,56 @@
-'use strict'
-
-let numberOfFilms;
-
-start();
+"use strict";
 
 let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-rememberMyFilms();
-detectPersonalLevel();
-showMyDB(personalMovieDB.privat);
-writeYourGenres();
-
-function start() {
-    numberOfFilms = +prompt("How many films have ou already seen?", "");
-
-    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("How many films have ou already seen?", "");
-    }
-}
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const movieName = prompt("One of the last movies you watched?", ""),
-            movieRate = prompt("How would you rate it?", "");
-    
-        if (movieName == null || movieRate == null ||
-            movieName == "" || movieRate == "" || 
-            movieName.length > 50) {
-            i--;
+	count: 0,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+	start: function() {
+		personalMovieDB.count = +prompt("How many films have ou already seen?", "");
+		while (personalMovieDB.count == "" || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+			personalMovieDB.count = +prompt("How many films have ou already seen?", "");
+		}
+	},
+	rememberMyFilms: function() {
+		for (let i = 0; i < 2; i++) {
+			const movieName = prompt("One of the last movies you watched?", ""),
+				movieRate = prompt("How would you rate it?", "");
+        
+			if (movieName == null || movieRate == null ||
+                movieName == "" || movieRate == "" || 
+                movieName.length > 50) {
+				i--;
+			} else {
+				personalMovieDB.movies[movieName] = movieRate;
+			}
+		}
+	},
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            alert("You have watched not enought movies.");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            alert("You are a good viewer.");
+        } else if (personalMovieDB.count >= 30) {
+            alert("You are a movie buff");
         } else {
-            personalMovieDB.movies[movieName] = movieRate;
+            alert("Error occured.");
+        }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 0; i <= 2; i++) {
+            personalMovieDB.genres[i] = prompt(`Your favourite genre ${i + 1}`);
         }
     }
-}
+};
 
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        alert("You have watched not enought movies.");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        alert("You are a good viewer.");
-    } else if (personalMovieDB.count >= 30) {
-        alert("You are a movie buff");
-    } else {
-        alert("Error occured.")
-    }
-}
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-function writeYourGenres() {
-    for (let i = 0; i <= 2; i++) {
-        personalMovieDB.genres[i] = prompt(`Your favourite genre ${i + 1}`);
-    }
-}
+personalMovieDB.start();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.showMyDB(personalMovieDB.privat);
+personalMovieDB.writeYourGenres();
